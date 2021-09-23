7482a718242e34699d96e15f83ea58a6d150bf25
Jose Alejandro Morán Pérez <jaleomp@gmail.com>
2021-09-23 11:05:10 +0200

    v0.1.1.1 Eliminada la dependencia de utils. Se sustituye la dependencia del paquete para disminuir los requerimientos del paquete. Ahora se usa el paquete clipr. Se exporta una nueva función convertirRutaDeRaFormatoWindows.

--- R/HerramientasDependientes.R
+++ R/HerramientasDependientes.R
@@ -93,25 +93,43 @@ desconectarTodasLasBasesDeDatosMysql <- function() {
 
 #' extraerRutaDeWindows
 #'
-#' Formatéa una ruta del sistema operativo Windows para que sea posible introducirla en un script de R sin necesidad e hacer cambios adicionales.
+#' Formatéa una ruta del sistema operativo Windows para que sea posible
+#' introducirla en un script de R sin necesidad e hacer cambios adicionales.
 #'
-#' Esta utilidad tiene un comportamiento especial: utiliza el contendio del portapapeles, por tanto, la ruta del dirctorio debe estar en el portapapeles.
+#' Esta utilidad tiene un comportamiento especial: utiliza el contendio del
+#' portapapeles, por tanto, la ruta del dirctorio debe estar en el portapapeles.
 #' Al ejecutrar esta función, se imprime la ruta en el formato que se necesita
 #' @author Jose Alejandro Morán Pérez
-#' @param path El texto de la variable que se quiere convertir. Por defecto, la cadena almacenada en el portapapeles.
-#' @param copiarAPortaPapeles Indica si copiar, o no, el resultado al portapapeles. Por defecto, si.
+#' @param path El texto de la variable que se quiere convertir. Por defecto, la
+#'   cadena almacenada en el portapapeles.
+#' @param copiarAPortaPapeles Indica si copiar, o no, el resultado al
+#'   portapapeles. Por defecto, si.
 #' @return Una ruta compatible con R.
-#' @examples extraerRutaDeWindows() #-> "G:/Unidades compartidas/EMPRESA/BBDD/JSON"
+#' @examples
+#' \dontrun{
+#' clipr::write_clip("G:\u005cUnidades compartidas\u005cEMPRESA\u005cBBDD\u005cJSON",
+#' allow_non_interactive = TRUE)
+#' # Los caracteres unicode son barras invertidas. R es así. Es un caracter reservado
+#' clipr::read_clip()
+#' # Esto saca por pantalla la ruta escapada. Pero en el portapapeles se
+#' # almacena bien, con una sóla barra inclinada. R no puede impirmir esto
+#' # porque lo interpreta como escape de códigos unicode. (R va a tener que
+#' # mejorar este comportamiento, pero mientra y no ...)
+#' extraerRutaDeWindows(copiarAPortaPapeles = FALSE)
+#' #-> "G:/Unidades compartidas/EMPRESA/BBDD/JSON"
+#' }
 #' @export
 extraerRutaDeWindows <- function(path = "clipboard", copiarAPortaPapeles = TRUE) {
   y <- if (path == "clipboard") {
-    readClipboard()
+    # readClipboard()
+    clipr::read_clip()
   } else {
     cat("Por favor, intorduce la ruta:\n\n")
     readline()
   }
   x <- chartr("\\", "/", y)
-  if (copiarAPortaPapeles) writeClipboard(x)
+  # if (copiarAPortaPapeles) writeClipboard(x)
+  if (copiarAPortaPapeles) clipr::write_clip(x, allow_non_interactive = TRUE)
 
   return(x)
 }
@@ -124,14 +142,17 @@ extraerRutaDeWindows <- function(path = "clipboard", copiarAPortaPapeles = TRUE)
 #' Al ejecutrar esta función, se imprime la ruta en el formato que se necesita
 #' @author Jose Alejandro Morán Pérez
 #' @param path La ruta en formato R.
-#' @importFrom utils readClipboard writeClipboard
+#' @param copiarAPortaPapeles Indica si copiar, o no, el resultado al portapapeles. Por defecto, si.
 #' @return Una ruta compatible con Windows.
-#' @examples extraerRutaDeWindows("G:/Unidades compartidas/EMPRESA/BBDD/JSON")
+#' @examples convertirRutaDeRaFormatoWindows("G:/Unidades compartidas/EMPRESA/BBDD/JSON", FALSE)
 #' #-> Produce: "G:\Unidades compartidas\EMPRESA\BBDD\JSON"
 #' # Si se quiere elimina la doble \ se debe imporimir este resultado con cat()
-convertirRutaDeRaFormatoWindows <- function(path = "clipboard") {
+#' @export
+convertirRutaDeRaFormatoWindows <- function(path = "clipboard", copiarAPortaPapeles = TRUE) {
+  #REM #' @importFrom utils readClipboard writeClipboard
   y <- if (path == "clipboard") {
-    readClipboard()
+    # readClipboard()
+    clipr::read_clip()
   } else if (nchar(path) > 0) {
     y <- path
   } else {
@@ -139,6 +160,7 @@ convertirRutaDeRaFormatoWindows <- function(path = "clipboard") {
     readline()
   }
   x <- chartr("/", "\\", y)
-  writeClipboard(x)
+  # writeClipboard(x)
+  if (copiarAPortaPapeles) clipr::write_clip(x, allow_non_interactive = TRUE)
   return(x)
 }
